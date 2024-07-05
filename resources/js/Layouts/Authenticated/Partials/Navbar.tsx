2679bd25fa433fb284701e8ac8fb30e45935b352
irealworlds <marincalin2551@hotmail.com>
2024-07-05 15:07:18 +0300

    Removed unused items from the navbar.

--- resources/js/Layouts/Authenticated/Partials/Navbar.tsx
+++ resources/js/Layouts/Authenticated/Partials/Navbar.tsx
@@ -1,8 +1,5 @@
-import { Bars3Icon, BellIcon } from '@heroicons/react/24/outline';
-import {
-    ChevronDownIcon,
-    MagnifyingGlassIcon,
-} from '@heroicons/react/20/solid';
+import { Bars3Icon } from '@heroicons/react/24/outline';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
 import { Menu, Transition } from '@headlessui/react';
 import { Fragment } from 'react';
 import { Link } from '@inertiajs/react';
@@ -39,39 +36,8 @@ export default function Navbar({
                 />
 
                 <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-                    <form
-                        className="relative flex flex-1"
-                        action="#"
-                        method="GET">
-                        <label htmlFor="search-field" className="sr-only">
-                            Search
-                        </label>
-                        <MagnifyingGlassIcon
-                            className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
-                            aria-hidden="true"
-                        />
-                        <input
-                            id="search-field"
-                            className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-                            placeholder="Search..."
-                            type="search"
-                            name="search"
-                        />
-                    </form>
+                    <div className="relative flex flex-1"></div>
                     <div className="flex items-center gap-x-4 lg:gap-x-6">
-                        <button
-                            type="button"
-                            className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
-                            <span className="sr-only">View notifications</span>
-                            <BellIcon className="size-6" aria-hidden="true" />
-                        </button>
-
-                        {/* Separator */}
-                        <div
-                            className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"
-                            aria-hidden="true"
-                        />
-
                         {/* Profile dropdown */}
                         <Menu as="div" className="relative">
                             <Menu.Button className="-m-1.5 flex items-center p-1.5">
--- resources/js/Layouts/Authenticated/Partials/Sidebar.tsx
+++ resources/js/Layouts/Authenticated/Partials/Sidebar.tsx
@@ -1,5 +1,3 @@
-import { Link } from '@inertiajs/react';
-import { Cog6ToothIcon } from '@heroicons/react/24/outline';
 import React from 'react';
 import { NavigationItem } from '@/types/layouts/authenticated/navigation-item.dto';
 import { NavigationCategory } from '@/types/layouts/authenticated/navigation-category.dto';
@@ -62,17 +60,6 @@ export default function Sidebar({
                                     )
                                 )}
                             </ul>
-                        </li>
-                        <li className="mt-auto">
-                            <Link
-                                href="#"
-                                className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
-                                <Cog6ToothIcon
-                                    className="h-6 w-6 shrink-0"
-                                    aria-hidden="true"
-                                />
-                                Settings
-                            </Link>
                         </li>
                     </ul>
                 </nav>
